d3aa927d2f80c471adbebbb9a4ebddc2bb2d5d1a
Mattia Tognela <tognelamattia@protonmail.com>
2025-08-09 17:27:48 +0200

    Add cImport for mpv-mpris headers in test-main.zig

--- test/zig/test-main.zig
+++ test/zig/test-main.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
 
+const c = @cImport({
+    @cInclude("mpv-mpris-artwork.h");
+    @cInclude("mpv-mpris-dbus.h");
+    @cInclude("mpv-mpris-events.h");
+    @cInclude("mpv-mpris-metadata.h");
+    @cInclude("mpv-mpris-types.h");
+});
+
 pub fn main() !void {}
 
 test "basic test" {
